0ce42ee796c90535fa6ef4efe126c252f2fa398d
zxiaoyong <zxiaoyong@gmail.com>
2012-03-12 08:18:18 +0000

    Refine connection's callback for async_read and async_write ONLY pass build

--- QuickArbitrage/TradeBuffer/connection.h
+++ QuickArbitrage/TradeBuffer/connection.h
@@ -8,6 +8,7 @@
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
 #include <boost/tuple/tuple.hpp>
+#include <boost/thread.hpp>
 
 
 class connection
@@ -30,12 +31,25 @@ public:
 	template <typename Handler>
 	void async_write(MSG_TYPE msg_type, std::string& data, Handler handler)
 	{
+		boost::unique_lock<boost::mutex> lock(_write_mutex);
+
+		while (!_ready_to_write)
+		{
+			_writable_cond.wait(lock);
+		}
+
+		_ready_to_write = false;
+
+		void (connection::*f)(
+			const boost::system::error_code&, std::size_t bytes_transferred, boost::tuple<Handler>)
+			= &connection::handle_write<Handler>;
+
 		// Serialize the data first so we know how large it is.
 		if(data.size() == 0)
 		{
 			// Something went wrong, inform the caller.
 			boost::system::error_code error(boost::asio::error::no_data);
-			socket_.get_io_service().post(boost::bind(handler, error));
+			socket_.get_io_service().post(boost::bind(f, this, error, 0, boost::make_tuple(handler)));
 			return;
 		}
 
@@ -48,7 +62,7 @@ public:
 		{
 			// Something went wrong, inform the caller.
 			boost::system::error_code error(boost::asio::error::invalid_argument);
-			socket_.get_io_service().post(boost::bind(handler, error));
+			socket_.get_io_service().post(boost::bind(f, this, error, 0, boost::make_tuple(handler)));
 			return;
 		}
 		outbound_header_ = header_stream.str();
@@ -58,35 +72,53 @@ public:
 		std::vector<boost::asio::const_buffer> buffers;
 		buffers.push_back(boost::asio::buffer(outbound_header_));
 		buffers.push_back(boost::asio::buffer(data));
-		boost::asio::async_write(socket_, buffers, handler);
+
+		boost::asio::async_write(socket_, buffers, 
+			boost::bind(f, this, _1, _2, boost::make_tuple(handler)));
+	}
+
+	/// Handle completion of a write operation.
+	// NOTE that MUST NOT call async_write in write completeion handler
+	template <typename Handler>
+	void handle_write(const boost::system::error_code& e, std::size_t bytes_transferred, boost::tuple<Handler> handler)
+	{
+		boost::get<0>(handler)(e, bytes_transferred);
+		{
+			boost::lock_guard<boost::mutex> lock(_write_mutex);
+			_ready_to_write = true;
+		}
+		_writable_cond.notify_one();
+	}
+
+	bool is_read_to_write()
+	{
+		boost::lock_guard<boost::mutex> lock(_write_mutex);
+		return _ready_to_write;
 	}
 
 	/// Asynchronously read a data structure from the socket.
 	template <typename Handler>
-	void async_read(MSG_TYPE* msg_type, std::string* pData, Handler handler)
+	void async_read(Handler handler)
 	{
 		// Issue a read operation to read exactly the number of bytes in a header.
 		void (connection::*f)(
-			const boost::system::error_code&, MSG_TYPE*,
-			std::string*, boost::tuple<Handler>)
+			const boost::system::error_code&, boost::tuple<Handler>)
 			= &connection::handle_read_header<Handler>;
 
 		boost::asio::async_read(socket_, boost::asio::buffer(inbound_header_),
-			boost::bind(f,
-			this, boost::asio::placeholders::error, msg_type, pData,
-			boost::make_tuple(handler)));
+			boost::bind(f, this, 
+				boost::asio::placeholders::error, boost::make_tuple(handler)));
 	}
 
 	/// Handle a completed read of a message header. The handler is passed using
 	/// a tuple since boost::bind seems to have trouble binding a function object
 	/// created using boost::bind as a parameter.
 	template <typename T, typename Handler>
-	void handle_read_header(const boost::system::error_code& e, MSG_TYPE* msg_type,
-		std::string* pData, boost::tuple<Handler> handler)
+	void handle_read_header(const boost::system::error_code& e, boost::tuple<Handler> handler)
 	{
 		if (e)
 		{
-			boost::get<0>(handler)(e);
+			handle_read(e, 0, handler);
 		}
 		else
 		{
@@ -94,57 +126,62 @@ public:
 			// Determine the length of the serialized data.
 			std::istringstream is(std::string(inbound_header_, header_length));
 			is >> std::hex >> nType;
-			*msg_type = static_cast<MSG_TYPE>(nType);
+			_in_msg_type = static_cast<MSG_TYPE>(nType);
 
 			std::size_t inbound_data_size = 0;
 			if (!(is >> std::hex >> inbound_data_size))
 			{
 				// Header doesn't seem to be valid. Inform the caller.
 				boost::system::error_code error(boost::asio::error::invalid_argument);
-				boost::get<0>(handler)(error);
+				handle_read(e, 0, handler);
 				return;
 			}
 
 			// Start an asynchronous call to receive the data.
 			inbound_data_.resize(inbound_data_size);
 			void (connection::*f)(
-				const boost::system::error_code&,
-				std::string*, boost::tuple<Handler>)
+				const boost::system::error_code&, std::size_t bytes_transferred, boost::tuple<Handler>)
 				= &connection::handle_read_data<Handler>;
+
 			boost::asio::async_read(socket_, boost::asio::buffer(inbound_data_),
 				boost::bind(f, this,
-				boost::asio::placeholders::error, pData, handler));
+					boost::asio::placeholders::error, 0, handler));
 		}
 	}
 
 	/// Handle a completed read of message data.
 	template <typename Handler>
-	void handle_read_data(const boost::system::error_code& e,
-		std::string* pData, boost::tuple<Handler> handler)
+	void handle_read_data(const boost::system::error_code& e, std::size_t bytes_transferred, boost::tuple<Handler> handler)
 	{
 		if (e)
 		{
-			boost::get<0>(handler)(e);
+			handle_read(e, bytes_transferred, handler);
 		}
 		else
 		{
 			// Extract the data structure from the data just received.
-			try
-			{
-				pData->assign(&inbound_data_[0], inbound_data_.size());
-			}
-			catch (std::exception& e)
-			{
-				// Unable to decode data.
-				boost::system::error_code error(boost::asio::error::invalid_argument);
-				boost::get<0>(handler)(error);
-				return;
-			}
+			_in_data.assign(&inbound_data_[0], inbound_data_.size());
 
-			// Inform caller that data has been received ok.
-			boost::get<0>(handler)(e);
+			handle_read(e, bytes_transferred, handler);
 		}
 	}
+
+	/// Handle completion of a read operation.
+	// NOTE that MUST NOT call async_read in read completeion handler
+	template <typename Handler>
+	void handle_read(const boost::system::error_code& e, std::size_t bytes_transferred, boost::tuple<Handler> handler)
+	{
+		if(e)
+		{
+			_in_msg_type = UNKNOWN;
+			_in_data = "";
+		}
+
+		boost::get<0>(handler)(e, _in_msg_type, _in_data);
+	}
+
+	MSG_TYPE get_in_msg_type(){ return _in_msg_type; }
+	std::string& get_in_data(){ return _in_data; }
 
 private:
 	/// The underlying socket.
@@ -157,6 +194,10 @@ private:
 		header_length = msg_type_length + data_size_length 
 	};
 
+	boost::mutex _write_mutex;
+	boost::condition_variable _writable_cond;
+	bool _ready_to_write;
+
 	/// Holds an outbound header.
 	std::string outbound_header_;
 
@@ -168,6 +209,10 @@ private:
 
 	/// Holds the inbound data.
 	std::vector<char> inbound_data_;
+
+	MSG_TYPE _in_msg_type;
+	std::string _in_data;
+
 };
 
 typedef boost::shared_ptr<connection> connection_ptr;
