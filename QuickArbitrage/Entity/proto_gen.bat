79affff3b632c441be501b365ec0514f834991dc
zxiaoyong <zxiaoyong@gmail.com>
2012-03-12 08:48:02 +0000

    Generate new entities for communication

--- QuickArbitrage/Entity/proto_gen.bat
+++ QuickArbitrage/Entity/proto_gen.bat
@@ -1,1 +1,7 @@
-protoc --cpp_out=../TradeBuffer/Protobuf ./quote.proto
+set Src=E:/Workspace/cpp-proto-net/QuickArbitrage/Entity
+set Dest=E:/Workspace/cpp-proto-net/QuickArbitrage/TradeBuffer/protobuf_gen
+protoc -I=%Src% --cpp_out=%Dest% %Src%/heartbeat.proto
+protoc -I=%Src% --cpp_out=%Dest% %Src%/login.proto
+protoc -I=%Src% --cpp_out=%Dest% %Src%/quote.proto
+protoc -I=%Src% --cpp_out=%Dest% %Src%/subscribe.proto
+pause
