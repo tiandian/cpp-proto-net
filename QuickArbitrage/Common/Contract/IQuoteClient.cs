d267214f18224a4075640fd41793f4ec9ff72cbb
zxiaoyong <zxiaoyong@gmail.com>
2012-03-14 04:55:22 +0000

    C# side is able to subscribe / unsubscribe quote, as well as receiving quotes Only coding completed

--- QuickArbitrage/Common/Contract/IQuoteClient.cs
+++ QuickArbitrage/Common/Contract/IQuoteClient.cs
@@ -7,5 +7,31 @@ namespace QuickArbitrage.Common.Contract
 {
     public interface IQuoteClient
     {
+        void Subscribe(IEnumerable<string> symbols);
+
+        void Unsubscribe(IEnumerable<string> symbols);
+
+        event EventHandler<OnQuoteReceivedEventArgs> OnQuoteReceived;
+    }
+
+    public class OnQuoteReceivedEventArgs : EventArgs
+    {
+        public Quote Quote { get; private set; }
+
+        public string Symbol
+        {
+            get 
+            {
+                if (Quote != null)
+                    return Quote.symbol;
+                else
+                    return string.Empty;
+            }
+        }
+
+        public OnQuoteReceivedEventArgs(Quote q)
+        {
+            this.Quote = q;
+        }
     }
 }
