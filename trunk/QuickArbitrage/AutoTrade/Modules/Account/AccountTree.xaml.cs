9e7c136ef10b5a3f6d432299b3d711f3944e761e
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2012-04-17 15:17:05 +0000

    Add a new project that targets to refactory Client UI with MEF, Prism, and DockManager

---
+++ trunk/QuickArbitrage/AutoTrade/Modules/Account/AccountTree.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel.Composition;
+
+namespace AutoTrade.MainFrame.Modules.Account
+{
+    /// <summary>
+    /// Interaction logic for AccountTree.xaml
+    /// </summary>
+    [Export]
+    public partial class AccountTree : UserControl
+    {
+        public AccountTree()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ trunk/QuickArbitrage/AutoTrade/Shell/ShellWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel.Composition;
+
+namespace AutoTrade.MainFrame
+{
+    /// <summary>
+    /// Interaction logic for ShellWindow.xaml
+    /// </summary>
+    [Export]
+    public partial class ShellWindow : Window
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string _savedLayout;
+
+        private void btnSaveLayout_Click(object sender, RoutedEventArgs e)
+        {
+            _savedLayout = dockManager.SaveLayout();
+        }
+
+        private void btnLoadLayout_Click(object sender, RoutedEventArgs e)
+        {
+            dockManager.LoadLayout(_savedLayout);
+        }
+    }
+}
