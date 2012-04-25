0a3d47b8dbffbb56b523f2bceb59881434d5dd06
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2012-04-25 15:30:32 +0000

    Add entity objects and view models

--- trunk/QuickArbitrage/AutoTrade/Shell/ShellViewModel.cs
+++ trunk/QuickArbitrage/AutoTrade/Shell/ShellViewModel.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Practices.Prism.ViewModel;
+using System.ComponentModel.Composition;
+using AutoTrade.MainFrame.ViewModels;
+using AutoTrade.MainFrame.Modules.Account;
 
 namespace AutoTrade.MainFrame
 {
+    [Export]
     class ShellViewModel : NotificationObject
     {
+        public DelegateCommand AddAccountCommand { get; private set; }
+        public DelegateCommand AddPortfolioCommand { get; private set; }
+
+        [Import]
+        private AccountManager AccountMgr { get; set; }
+
+        public ShellViewModel()
+        {
+            InitCommands();
+        }
+
+        public Func<System.Windows.Window> GetOwner { get; set; }
+
+        private void InitCommands()
+        {
+            AddAccountCommand = new DelegateCommand(OnAddAccount);
+        }
+
+        public void OnAddAccount(object param)
+        {
+            AddAccountDlg dlg = new AddAccountDlg();
+            if (GetOwner != null)
+            {
+                dlg.Owner = GetOwner();
+            }
+            dlg.ShowDialog();
+        }
+
+        public void OnAddPortfolio(object param)
+        {
+
+        }
     }
 }
---
+++ trunk/QuickArbitrage/Common/DataEntity/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoBuf;
+
+namespace QuickArbitrage.Common.DataEntity
+{
+    [ProtoContract]
+    public class Account
+    {
+        [ProtoMember(1)]
+        public string AccountID { get; set; }
+
+        [ProtoMember(2)]
+        public string Username { get; set; }
+
+        [ProtoMember(3)]
+        public Portfolio[] Portfolios { get; set;}
+    }
+}
