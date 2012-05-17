38783e0f7c3822cc38119e2249a33e8cea12125b
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2012-05-17 13:27:10 +0000

    Release v1.0.2. Solve XP compatibility problem.

--- trunk/HiFreTrade/ShFuGateway/dllmain.cpp
+++ trunk/HiFreTrade/ShFuGateway/dllmain.cpp
@@ -25,7 +25,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		if(IsVistaOrLater())
+		//if(IsVistaOrLater())
 			logger.Init();
 		break;
 	case DLL_THREAD_ATTACH:
