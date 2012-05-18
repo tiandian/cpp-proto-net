89ca10958cb47ac0d40bdf2032d0b64a06e0a655
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2012-05-18 15:03:37 +0000

    Add splash screen and login window

---
+++ trunk/HiFreTrade/HiFreTradeUI/LoginWnd.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HiFreTradeUI
+{
+    /// <summary>
+    /// Interaction logic for LoginWnd.xaml
+    /// </summary>
+    public partial class LoginWnd : Window
+    {
+        public LoginWnd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+    }
+}
---
+++ trunk/HiFreTrade/HiFreTradeUI/ViewModels/LoginVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.ViewModel;
+
+namespace HiFreTradeUI.ViewModels
+{
+    public class LoginVM : NotificationObject
+    {
+        #region IsLogingin
+        private bool _isLogingin;
+
+        public bool IsLogingin
+        {
+            get { return _isLogingin; }
+            set
+            {
+                if (_isLogingin != value)
+                {
+                    _isLogingin = value;
+                    RaisePropertyChanged("IsLogingin");
+                }
+            }
+        }
+        #endregion
+
+        #region AccountId
+        private string _accountId;
+
+        public string AccountId
+        {
+            get { return _accountId; }
+            set
+            {
+                if (_accountId != value)
+                {
+                    _accountId = value;
+                    RaisePropertyChanged("AccountId");
+                }
+            }
+        }
+        #endregion
+
+    }
+}
