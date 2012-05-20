c075697c4eba454f783ba7a6b7cd37c293431b8b
zxiaoyong <zxiaoyong@gmail.com>
2012-05-20 15:00:52 +0000

    add IF1207

--- HiFreTrade/HiFreTradeUI/ShellViewModel.cs
+++ HiFreTrade/HiFreTradeUI/ShellViewModel.cs
@@ -17,8 +17,8 @@ namespace HiFreTradeUI
 
         public ShellViewModel()
         {
-            _availableSymbols.Add("IF1205");
             _availableSymbols.Add("IF1206");
+            _availableSymbols.Add("IF1207");
             _availableSymbols.Add("IF1209");
             _availableSymbols.Add("IF1212");
 
