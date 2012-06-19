7a34373070da28b4a1c2624cb9db5cd557f54e80
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2012-06-19 14:01:50 +0000

    Change test timeout to 15 secs. Be able to cancel connection test

--- trunk/HiFreTrade/HiFreTradeUI/TestConnectionWnd.xaml.cs
+++ trunk/HiFreTrade/HiFreTradeUI/TestConnectionWnd.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using HiFreTradeUI.Win32Invoke;
 using HiFreTradeUI.ServerConfig;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace HiFreTradeUI
 {
@@ -20,22 +22,64 @@ namespace HiFreTradeUI
     /// </summary>
     public partial class TestConnectionWnd : Window
     {
+        private BackgroundWorker _bgWorker;
+
+        private bool _testing = false;
+        
         public TestConnectionWnd(ServerConfiguation servConfig)
         {
             this.DataContext = servConfig;
             InitializeComponent();
+
+            _bgWorker = new BackgroundWorker();
+            _bgWorker.WorkerSupportsCancellation = true;
+            _bgWorker.DoWork += new DoWorkEventHandler(_bgWorker_DoWork);
+            _bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_bgWorker_RunWorkerCompleted);
         }
 
+        void _bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                Debug.WriteLine("Test cancelled");
+            }
+            else
+            {
+                Debug.WriteLine("Test finished");
+            }
+
+            this.Dispatcher.Invoke(
+                new Action(
+                    delegate
+                    {
+                        _testing = false;
+                        this.btnTest.Content = "测试连接";
+                        this.btnTest.IsEnabled = true;
+                    }));
+            
+        }
+
+        void _bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ServerGroup group = e.Argument as ServerGroup;
+            TestServers(group.Servers, e);
+        }
+
+        
         private void btnTest_Click(object sender, RoutedEventArgs e)
         {
-            ServerGroup group = this.cmbServGroup.SelectedItem as ServerGroup;
-
-            Action<IEnumerable<ServerInfo>> testFunc = TestServers;
-            testFunc.BeginInvoke(group.Servers, 
-                delegate(IAsyncResult ar)
-                {
-                    testFunc.EndInvoke(ar);
-                }, null);
+            if (_testing)
+            {
+                _bgWorker.CancelAsync();
+                this.btnTest.Content = "正在停止";
+                this.btnTest.IsEnabled = false;
+            }
+            else
+            {
+                _bgWorker.RunWorkerAsync(this.cmbServGroup.SelectedItem);
+                _testing = true;
+                this.btnTest.Content = "停止测试";
+            }
         }
 
         private void UpdateTestStatus(ServerInfo si, ServerStatus status)
@@ -43,11 +87,18 @@ namespace HiFreTradeUI
             this.Dispatcher.Invoke(new Action(()=> si.Status = status));
         }
 
-        private void TestServers(IEnumerable<ServerInfo> servers)
+        private void TestServers(IEnumerable<ServerInfo> servers, DoWorkEventArgs e)
         {
             foreach (var si in servers)
             {
+                if (_bgWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+
                 UpdateTestStatus(si, ServerStatus.Testing);
+
                 int delay = TestAddress(si.Address);
                 if(delay > 0)
                     UpdateTestStatus(si, ServerStatus.Available);
