be8f3977a772ed7609746b351de6a7c094e4fc38
zxiaoyong <zxiaoyong@gmail.com>
2012-06-27 14:08:58 +0000

    Add TradeStation project

---
+++ PortfolioTrading/TradeStation/TradeStation.cpp
@@ -0,0 +1,16 @@
+// TradeStation.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#ifdef _DEBUG
+#pragma comment(lib, "../Debug/APInvokeNative.lib")
+#else
+#pragma comment(lib, "../Release/APInvokeNative.lib")
+#endif
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
