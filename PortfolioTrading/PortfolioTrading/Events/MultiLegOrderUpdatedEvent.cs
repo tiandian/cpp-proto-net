69db07eb5c848749d80bbbde6691501854e01a83
zxiaoyong <zxiaoyong@gmail.com>
2012-09-02 15:00:01 +0000

    Be able to store and update MultiLegOrder pushed

---
+++ PortfolioTrading/PortfolioTrading/Events/MultiLegOrderUpdatedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Events;
+
+namespace PortfolioTrading.Events
+{
+    public class MultiLegOrderUpdatedEvent : CompositePresentationEvent<trade.MultiLegOrder>
+    {
+    }
+}
