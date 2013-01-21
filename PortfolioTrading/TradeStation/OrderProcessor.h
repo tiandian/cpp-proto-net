f5cb009bd444e3db89b2d21978588b5e013de223
zxiaoyong <zxiaoyong@gmail.com>
2013-01-21 15:05:48 +0000

    Add ManualSgOrderPlacer

--- PortfolioTrading/TradeStation/OrderProcessor.h
+++ PortfolioTrading/TradeStation/OrderProcessor.h
@@ -33,7 +33,7 @@ public:
 
 	void SubmitOrder(MultiLegOrderPtr multilegOrder);
 	void SubmitOrder2(MultiLegOrderPtr multilegOrder, bool autoTracking);
-	trade::InputOrder* BuildSingleOrder(const string& symbol, trade::TradeDirectionType direction, trade::OffsetFlagType offsetFlag, PlaceOrderContext* placeOrderCtx);
+	trade::InputOrder* BuildCloseOrder(const string& symbol, trade::TradeDirectionType direction, trade::OffsetFlagType offsetFlag, PlaceOrderContext* placeOrderCtx);
 	void CancelOrder(	const std::string& ordRef, 
 						const std::string& exchId, 
 						const std::string& ordSysId, 
