84d2674949c88b2bbdfd727e6500448f90cfc326
zxiaoyong <zxiaoyong@gmail.com>
2013-05-15 15:47:50 +0000

    Have FastTradeStation be able to launch QuoteStation with changed QuoteAgent. Advance to 1.3

--- PortfolioTrading/FastTradeStation/QuoteAgent.h
+++ PortfolioTrading/FastTradeStation/QuoteAgent.h
@@ -1,16 +1,20 @@
 #pragma once
 
+#include "ShmQuoteSubscribe.h"
+#include "ShmQuoteFeed.h"
 #include "QuoteAgentCallback.h"
-#include "ThostTraderApi/ThostFtdcMdApi.h"
+#include "ThostTraderApi/ThostFtdcUserApiDataType.h"
 
 #include <string>
 #include <vector>
 #include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
 #include <boost/tuple/tuple.hpp>
+#include <boost/atomic.hpp>
 
 using namespace std;
 
-class CQuoteAgent : public CThostFtdcMdSpi
+class CQuoteAgent
 {
 public:
 	CQuoteAgent(void);
@@ -30,67 +34,22 @@ public:
 	bool IsConnected(){ return m_bIsConnected; }
 	bool IsDisconnected(){ return !m_bIsConnected; }
 
-	//////////////////////////////////////////////////////////////////////////
-
-	///当客户端与交易后台建立起通信连接时（还未登录前），该方法被调用。
-	virtual void OnFrontConnected();
-
-	///当客户端与交易后台通信连接断开时，该方法被调用。当发生这个情况后，API会自动重新连接，客户端可不做处理。
-	///@param nReason 错误原因
-	///        0x1001 网络读失败
-	///        0x1002 网络写失败
-	///        0x2001 接收心跳超时
-	///        0x2002 发送心跳失败
-	///        0x2003 收到错误报文
-	virtual void OnFrontDisconnected(int nReason);
-
-	///心跳超时警告。当长时间未收到报文时，该方法被调用。
-	///@param nTimeLapse 距离上次接收报文的时间
-	virtual void OnHeartBeatWarning(int nTimeLapse);
-
-	///登录请求响应
-	virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-
-	///登出请求响应
-	virtual void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-
-	///错误应答
-	virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-
-	///订阅行情应答
-	virtual void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-
-	///取消订阅行情应答
-	virtual void OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
-
-	///深度行情通知
-	virtual void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData);
-
-	//////////////////////////////////////////////////////////////////////////
-
 private:
-	int RequestIDIncrement();
-	// 请求编号
-	int m_iRequestID;
-	boost::mutex m_mutReqIdInc;
+	void OnQuotePush(CThostFtdcDepthMarketDataField* mktDataField);
+	void LaunchChildProc(string cmd);
 
-	boost::condition_variable m_condLogin;
-	boost::mutex m_mutLogin;
-	string m_sLoginError;
-	bool m_loginSuccess;
-
-	CThostFtdcMdApi* m_pUserApi;
 	CQuoteAgentCallback* m_pCallback;
 
-	bool m_bIsConnected;
-	bool m_bConnectionDrop;
-	bool m_bIsConnecting;
+	boost::atomic<bool> m_bIsConnected;
+	int m_retChild;
+	boost::thread m_thLaunch;
 
-	boost::condition_variable m_condConnectDone;
-	boost::mutex m_mutex;
-	boost::thread m_thQuoting;
-
+	string m_connString;
 	string m_brokerID;
 	string m_userID;
+
+	boost::shared_ptr<CShmQuoteSubscribeProducer> m_quoteSubscriber;
+	boost::shared_ptr<CShmQuoteFeedConsumer> m_quoteFeedee;
+	
 };
 
