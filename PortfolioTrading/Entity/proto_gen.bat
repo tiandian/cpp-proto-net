256c0c2ff926422d08d45464b2ca3037795a7f6e
zxiaoyong <zxiaoyong@gmail.com>
2013-05-23 14:50:37 +0000

    Add Heartbeat to Packet

--- PortfolioTrading/Entity/proto_gen.bat
+++ PortfolioTrading/Entity/proto_gen.bat
@@ -6,6 +6,7 @@ protoc -I=%Src% --cpp_out=%Dest% %Src%\enum.proto
 protoc -I=%Src% --cpp_out=%Dest% %Src%\message.proto
 protoc -I=%Src% --cpp_out=%Dest% %Src%\quote.proto
 protoc -I=%Src% --cpp_out=%Dest% %Src%\trade.proto
+%protobuf-net%\protogen -i:%Src%\packet.proto -o:.\gen\cs\Packet.cs
 %protobuf-net%\protogen -i:%Src%\enum.proto -o:.\gen\cs\Enums.cs
 %protobuf-net%\protogen -i:%Src%\message.proto -o:.\gen\cs\Entity.cs
 %protobuf-net%\protogen -i:%Src%\quote.proto -o:.\gen\cs\Quote.cs
