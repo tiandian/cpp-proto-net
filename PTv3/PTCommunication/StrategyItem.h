ee46b2dac0b7198e7e8676f061c54e0a6a97ff41
zxiaoyong <zxiaoyong@gmail.com>
2013-07-01 14:35:52 +0000

    Allow to transfer Strategy and trigger from managed runtime to native runtime via C++/CLI

--- PTv3/PTCommunication/StrategyItem.h
+++ PTv3/PTCommunication/StrategyItem.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include "Enums.h"
+#include "TriggerItem.h"
 #include "entity/message.pb.h"
 
 using namespace System;
 using namespace System::Collections::Generic;
+using namespace System::Runtime::InteropServices;
 
 namespace PTEntity {
 
@@ -12,7 +15,53 @@ public ref class StrategyItem
 public:
 	StrategyItem(void);
 
-	void To(entity::StrategyItem* pNativeStrategyItem);
+	virtual void To(entity::StrategyItem* pNativeStrategyItem);
+
+protected:
+	StrategyType _type;
+	bool _running;
+	int _retryTimes;
+	List<TriggerItem^> ^_triggers;
+};
+
+public ref class ArbitrageStrategyItem : StrategyItem
+{
+public:
+	ArbitrageStrategyItem(){}
+
+	virtual void To(entity::StrategyItem* pNativeStrategyItem) override;
+
+private:
+	PosiDirectionType _side;
+};
+
+public ref class ChangePositionStrategyItem : StrategyItem
+{
+public:
+	ChangePositionStrategyItem(){}
+
+	virtual void To(entity::StrategyItem* pNativeStrategyItem) override;
+
+private:
+	String ^_closeLeg;
+	PosiDirectionType _closeLegSide;
+};
+
+public ref class ScalperStrategyItem : StrategyItem
+{
+public:
+	ScalperStrategyItem(){}
+
+	virtual void To(entity::StrategyItem* pNativeStrategyItem) override;
+
+private:
+	double _priceTick;
+	DirectionDepends _caseLE2Tick;
+	DirectionDepends _caseLE3Tick;
+	DirectionDepends _caseGE4Tick;
+	DirectionDepends _caseNoChange;
+	int _openTimeout;
+	StopLossCloseMethods _stopLossStrategy;
 };
 
 }
