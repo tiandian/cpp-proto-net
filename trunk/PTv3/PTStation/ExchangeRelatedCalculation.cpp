b26dae722e6f829338169d7babc6515ea8769b8f
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2013-09-02 07:56:21 +0000

    Edit ExchangeRelatedCalculation.cpp from Web

---
+++ trunk/PTv3/PTStation/ExchangeRelatedCalculation.cpp
@@ -0,0 +1,43 @@
+#include "StdAfx.h"
+#include "ExchangeRelatedCalculation.h"
+
+#include <boost/algorithm/string.hpp>
+
+boost::chrono::seconds ParseTimeString(const char* time)
+{
+    boost::chrono::seconds finalVal;
+    unsigned int hour = 0, min = 0, sec = 0;
+    int got = sscanf(time, "%u:%u:%u", &hour, &min, &sec);
+    if(got == 3)
+    {
+        finalVal = boost::chrono::hours(hour)
+            + boost::chrono::minutes(min)
+            + boost::chrono::seconds(sec);
+    }
+    // else must be something wrong
+    return finalVal;
+}
+
+boost::chrono::seconds ParseTimeString(const string& time)
+{
+    return ParseTimeString(time.c_str());
+}
+
+bool isSymbolIF(const string& symbol)
+{
+    return boost::starts_with(symbol, "IF");
+}
+
+unsigned int GetIndexFromTime(const boost::chrono::seconds& baseTp, const boost::chrono::seconds& timePoint, unsigned int precision)
+{
+    boost::chrono::seconds diff = timePoint - baseTp;
+    if(diff > boost::chrono::seconds::zero())
+    {
+        unsigned int idx = diff.count() / precision;
+        if(diff.count() % precision > 0)
+            ++idx;
+
+        return idx;
+    }
+    return 0;
+}
