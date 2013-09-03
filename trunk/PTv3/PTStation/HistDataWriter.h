6e08f6c85928fe2be85dadf3366e63eda880ad76
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2013-09-03 09:24:24 +0000

    Edit HistDataWriter via Web

--- trunk/PTv3/PTStation/HistDataWriter.h
+++ trunk/PTv3/PTStation/HistDataWriter.h
@@ -1,10 +1,19 @@
 #pragma once
+
+#include <boost/filesystem/fstream.hpp>
+
 class CHistDataWriter
 {
 public:
-	CHistDataWriter(void);
-	~CHistDataWriter(void);
+    CHistDataWriter(void);
+    ~CHistDataWriter(void);
+    bool Open(const string& symbol, unsigned int precision);
+    bool IsOpen(){ return m_ofstream.is_open(); }
+    void Close();
+    void Write(const string& timestamp, double open, double high, double low, double close);
 
-	void Write(double open, double high, double low, double close){}
+private:
+    string m_symbol;
+    unsigned int m_precision;
+    boost::filesystem::ofstream m_ofstream;
 };
-
