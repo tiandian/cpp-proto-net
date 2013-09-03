2d1e5bbf429c398ae9922ce9ee15d76a86513321
zxiaoyong <zxiaoyong@gmail.com>
2013-09-03 09:25:14 +0000

    Edit HistDataWriter.cpp via Web

--- PTv3/PTStation/HistDataWriter.cpp
+++ PTv3/PTStation/HistDataWriter.cpp
@@ -1,6 +1,8 @@
 #include "StdAfx.h"
 #include "HistDataWriter.h"
+#include "TechStrategyDefs.h"
 
+using namespace boost::filesystem;
 
 CHistDataWriter::CHistDataWriter(void)
 {
@@ -9,4 +11,32 @@ CHistDataWriter::CHistDataWriter(void)
 
 CHistDataWriter::~CHistDataWriter(void)
 {
+    if(IsOpen())
+        Close();
+}
+
+bool CHistDataWriter::Open(const string& symbol, unsigned int precision)
+{
+    m_symbol = symbol;
+    m_precision = precision;
+    string filePath = boost::str(boost::format("%s/%s-%u.txt") % HIST_DATA_FOLDER % m_symbol % m_precision);
+    m_ofstream.open(filePath, std::ofstream::out | std::ofstream::app);
+    return m_ofstream.is_open();
+}
+
+void CHistDataWriter::Close()
+{
+    m_ofstream.close();
+}
+
+void CHistDataWriter::Write(const string& timestamp, double open, double high, double low, double close)
+{
+    if(IsOpen())
+    {
+        m_ofstream << timestamp << " ";
+        m_ofstream << open << ", ";
+        m_ofstream << high << ", ";
+        m_ofstream << low << ", ";
+        m_ofstream << close << endl;
+    }
 }
