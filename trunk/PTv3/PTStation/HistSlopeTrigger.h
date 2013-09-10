d02cef7d9325f83b4f90d69bfe2755f98a02993d
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2013-09-10 09:23:33 +0000

    Edit HistSlopeTrigger.h via Web for Trailing Stop

--- trunk/PTv3/PTStation/HistSlopeTrigger.h
+++ trunk/PTv3/PTStation/HistSlopeTrigger.h
@@ -14,3 +14,60 @@ private:
 	entity::PosiOffsetFlag m_offset;
 };
 
+class CHistSlopeTrailingStop : public CTrigger
+{
+public:
+    CHistSlopeTrailingStop()
+        :CTrigger("TrailingStop"){}
+    ~CHistSlopeTrailingStop(){}
+
+    void Enable(double cost, LongShort direction)
+    {
+        m_lastHigh = cost;
+        m_effectiveStop = CalcOffset(cost, -m_backValue);
+        m_direction = direction;
+        CTrigger::Enable(true);
+    }
+
+    void Apply()
+    {
+        // m_backValue = 
+    }
+
+    // Input value should be latest price
+    bool OnTest(double val)
+    { 
+        if(Compare(val, m_lastHigh))
+        {
+            m_lastHigh = val;
+            m_effectiveStop = CalcOffset(m_lastHigh, -m_backValue);
+        }
+        else if(!Compare(val, m_effectiveStop))
+        {
+            return true;
+        }
+
+        return false; 
+    }
+
+private:
+
+    double CalcOffset(double base, double offset)
+    {
+        return m_direction == LS_LONG ? 
+            base + offset : base - offset;
+    }
+
+    bool Compare(double src, double dest)
+    {
+        return m_direction == LS_LONG ?
+            src > dest : src < dest;
+    }
+
+    double m_backValue;
+    double m_effectiveStop;
+    double m_lastHigh;
+
+    LongShort m_direction;
+};
+
