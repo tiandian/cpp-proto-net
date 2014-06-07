845b67dca0148eee2bd6206edfe3438f5ab74521
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2014-06-07 15:17:38 +0000

    Portfolio Arbitrage Order Placer Pass build

--- trunk/PTv3/PTStation/PortfolioArbitrageOrderPlacer.h
+++ trunk/PTv3/PTStation/PortfolioArbitrageOrderPlacer.h
@@ -10,12 +10,13 @@ public:
 
 	virtual void BuildTemplateOrder();
 
-	void OpenPosition();
-	void ClosePosition();
+	void OpenPosition(entity::PosiDirectionType posiDirection, double* pLmtPxArr, int iPxSize, const boost::chrono::steady_clock::time_point& trigQuoteTimestamp);
+	void ClosePosition(entity::PosiDirectionType posiDirection, double* pLmtPxArr, int iPxSize, const boost::chrono::steady_clock::time_point& trigQuoteTimestamp);
 	bool IsOpened();
 
-  void SetOffset(trade::trade::OffsetFlagType offset);
+	void Run(entity::PosiDirectionType posiDirection, trade::OffsetFlagType offset, double* pLmtPxArr, int iPxSize, const boost::chrono::steady_clock::time_point& trigQuoteTimestamp);
 private:
 
+	CLegOrderPlacer* GetLegOrderPlacer(const string& symbol);
 };
 
