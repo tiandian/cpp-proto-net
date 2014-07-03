0c98d43fc8a6f360e0f68c8792f42b776110ffad
zxiaoyong <zxiaoyong@gmail.com>
2014-07-03 15:10:17 +0000

    Improve bollinger band calculation

--- PTv3/PTStation/BollDataSet.cpp
+++ PTv3/PTStation/BollDataSet.cpp
@@ -32,8 +32,6 @@ void CBollDataSet::Calculate( COHLCRecordSet* ohlcRecordSet )
 
 	//CalculateTA(ohlcRecordSet, lastIdx);
 	CalculateRaw((ohlcRecordSet->CloseSeries).get(), lastIdx);
-
-	m_lastPosition = lastIdx;
 }
 
 void CBollDataSet::Calculate( CDiffRecordSet* diffRecordSet )
@@ -44,9 +42,8 @@ void CBollDataSet::Calculate( CDiffRecordSet* diffRecordSet )
 	logger.Info(boost::str(boost::format("Calculating BOLL with DiffRecordSet: lastIdx - %d, last price - %f")
 		% lastIdx % (diffRecordSet->DiffSeries)[lastIdx]));
 
+	//CalculateTA((diffRecordSet->DiffSeries).get(), lastIdx);
 	CalculateRaw((diffRecordSet->DiffSeries).get(), lastIdx);
-
-	m_lastPosition = lastIdx;
 }
 
 void CBollDataSet::CalculateRaw( double* pxArray, int lastIdx )
@@ -56,25 +53,54 @@ void CBollDataSet::CalculateRaw( double* pxArray, int lastIdx )
 	double outSma = 0;
 	double outStdDev = 0;
 
-	TA_RetCode rc = TA_SMA(lastIdx, lastIdx, pxArray, m_paramM, &outBeg, &outNbElement, &outSma);
-	if(outBeg == lastIdx)
-		m_arrMid[outBeg] = outSma;
-
-	rc = TA_STDDEV(lastIdx, lastIdx, pxArray, m_paramM, (double)m_paramP,
-		&outBeg, &outNbElement, &outStdDev);
-	if(outBeg == lastIdx)
+	if(m_lastPosition > 0)
 	{
-		m_arrTop[outBeg] = outSma + outStdDev;
-		m_arrBottom[outBeg] = outSma - outStdDev;
+		TA_RetCode rc = TA_SMA(lastIdx, lastIdx, pxArray, m_paramM, &outBeg, &outNbElement, &outSma);
+		if(outBeg == lastIdx)
+			m_arrMid[outBeg] = outSma;
 
-		logger.Info(boost::str(boost::format("Calculated BOLL values: mid - %.2f, top - %.2f, bottom - %.2f")
-			% m_arrMid[outBeg] % m_arrTop[outBeg] % m_arrBottom[outBeg] ));
+		rc = TA_STDDEV(lastIdx, lastIdx, pxArray, m_paramM, (double)m_paramP,
+			&outBeg, &outNbElement, &outStdDev);
+		if(outBeg == lastIdx)
+		{
+			m_arrTop[outBeg] = outSma + outStdDev;
+			m_arrBottom[outBeg] = outSma - outStdDev;
+
+			logger.Info(boost::str(boost::format("Calculated BOLL values: mid - %.2f, top - %.2f, bottom - %.2f")
+				% m_arrMid[outBeg] % m_arrTop[outBeg] % m_arrBottom[outBeg] ));
+		}
+		else
+		{
+			logger.Warning(boost::str(boost::format("Cannot calculate STDDEV !!! lastIdx:%d, outBeg:%d")
+				% lastIdx % outBeg));
+		}
 	}
 	else
 	{
-		logger.Warning(boost::str(boost::format("Cannot calculate STDDEV !!! lastIdx:%d, outBeg:%d")
-			% lastIdx % outBeg));
+		int sma_lookback = TA_SMA_Lookback(m_paramM);
+		TA_RetCode rc = TA_SMA(0, lastIdx, pxArray, m_paramM, &outBeg, &outNbElement,
+			m_arrMid + sma_lookback);
+		if(outBeg == sma_lookback)
+		{
+			int dev_lookback = TA_STDDEV_Lookback(m_paramM, (double)m_paramP);
+			DataSeries stdDevSeries(new double[m_size]);
+			ResetArray(stdDevSeries.get(), m_size, 0);
+			TA_RetCode rc = TA_STDDEV(0, lastIdx, pxArray, m_paramM, (double)m_paramP, &outBeg, &outNbElement,
+				stdDevSeries.get() + dev_lookback);
+			if(outBeg != dev_lookback)
+				logger.Warning(boost::str(boost::format("Calculated MA on WMA line may NOT be correct!!!")));
+			else
+			{
+				for(int i = 0; i < lastIdx; ++i)
+				{
+					m_arrTop[i] = m_arrMid[i] + stdDevSeries[i];
+					m_arrBottom[i] = m_arrMid[i] - stdDevSeries[i];
+				}
+			}
+		}
 	}
+
+	m_lastPosition = lastIdx;
 }
 
 void CBollDataSet::CalculateTA( double* pxArray, int lastIdx )
@@ -85,16 +111,31 @@ void CBollDataSet::CalculateTA( double* pxArray, int lastIdx )
 	double outTop = 0;
 	double outBottom = 0;
 
-	TA_RetCode rc = TA_BBANDS(lastIdx, lastIdx, pxArray, 
-		m_paramM, 2.0, 2.0, TA_MAType_SMA, &outBeg, &outNbElement, &outTop, &outMid, &outBottom);
-
-	if(outBeg == lastIdx)
+	if(m_lastPosition > 0)
 	{
-		m_arrTop[outBeg] = outTop;
-		m_arrBottom[outBeg] = outBottom;
-		m_arrMid[outBeg] = outMid;
+		TA_RetCode rc = TA_BBANDS(lastIdx, lastIdx, pxArray, 
+			m_paramM, (double)m_paramP, (double)m_paramP, TA_MAType_SMA, &outBeg, &outNbElement, &outTop, &outMid, &outBottom);
 
-		logger.Info(boost::str(boost::format("Calculated BOLL values: mid - %.2f, top - %.2f, bottom - %.2f")
-			% m_arrMid[outBeg] % m_arrTop[outBeg] % m_arrBottom[outBeg] ));
+		if(outBeg == lastIdx)
+		{
+			m_arrTop[outBeg] = outTop;
+			m_arrBottom[outBeg] = outBottom;
+			m_arrMid[outBeg] = outMid;
+
+			logger.Info(boost::str(boost::format("Calculated BOLL values: mid - %.2f, top - %.2f, bottom - %.2f")
+				% m_arrMid[outBeg] % m_arrTop[outBeg] % m_arrBottom[outBeg] ));
+		}
 	}
+	else
+	{
+		int boll_lookback = TA_BBANDS_Lookback(m_paramM, (double)m_paramP, (double)m_paramP, TA_MAType_SMA);
+		TA_RetCode rc = TA_BBANDS(0, lastIdx, pxArray, m_paramM, (double)m_paramP, (double)m_paramP, TA_MAType_SMA, 
+			&outBeg, &outNbElement, m_arrTop + boll_lookback, m_arrMid + boll_lookback, m_arrBottom + boll_lookback);
+		if(outBeg != boll_lookback)
+		{
+			logger.Warning(boost::str(boost::format("Calculated Boll Band may NOT be correct!!!")));
+		}
+	}
+
+	m_lastPosition = lastIdx;
 }
