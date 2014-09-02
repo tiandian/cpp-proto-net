37a7d8a1fc91b31aa8f72e602a97e467d870e274
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2014-09-02 11:49:46 +0000

    submit Infragistics assemblies

--- trunk/PTv3/PTClientUI/Properties/AssemblyInfo.cs
+++ trunk/PTv3/PTClientUI/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.0.407")]
-[assembly: AssemblyFileVersion("3.4.0.407")]
+[assembly: AssemblyVersion("4.0.0.825")]
+[assembly: AssemblyFileVersion("4.0.0.825")]
