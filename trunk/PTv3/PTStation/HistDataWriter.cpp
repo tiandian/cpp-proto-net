2371b71df9c2d8fbe0e1f014533b6fef9f0c51e7
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2014-10-13 15:13:57 +0000

    Make HistDataWriter to be singleton. Make HistDataWriter output good format. Use opposite price when cancel and retry.

--- trunk/PTv3/PTStation/HistDataWriter.cpp
+++ trunk/PTv3/PTStation/HistDataWriter.cpp
@@ -17,15 +17,30 @@ CHistDataWriter::~CHistDataWriter(void)
         Close();
 }
 
+void CHistDataWriter::Init(const string& symbol, int precision, const boost::gregorian::date& tradingDay)
+{
+	m_symbol = symbol;
+	m_precision = precision;
+	string tradingDayString = boost::gregorian::to_iso_string(tradingDay);
+	m_Name = boost::str(boost::format("%s-%u-%s") % m_symbol % m_precision % tradingDayString);
+}
+
+bool CHistDataWriter::Open()
+{
+	if (!m_Name.empty())
+	{
+		string filePath = boost::str(boost::format("%s/%s.txt")
+			% HIST_DATA_FOLDER % m_Name);
+		m_ofstream.open(filePath, std::ofstream::out | std::ofstream::app);
+		return m_ofstream.is_open();
+	}
+	return false;
+}
+
 bool CHistDataWriter::Open(const string& symbol, int precision, const boost::gregorian::date& tradingDay)
 {
-    m_symbol = symbol;
-    m_precision = precision;
-	string tradingDayString = boost::gregorian::to_iso_string(tradingDay);
-    string filePath = boost::str(boost::format("%s/%s-%u-%s.txt") 
-        % HIST_DATA_FOLDER % m_symbol % m_precision % tradingDayString);
-    m_ofstream.open(filePath, std::ofstream::out | std::ofstream::app);
-    return m_ofstream.is_open();
+	Init(symbol, precision, tradingDay);
+	return Open();
 }
 
 void CHistDataWriter::Close()
@@ -37,8 +52,8 @@ void CHistDataWriter::Write(const string& timestamp, double open, double high, d
 {
     if(IsOpen())
     {
-        m_ofstream << timestamp << " , ";
-        m_ofstream << setw(FIELD_WIDTH) << open << ", ";
+		m_ofstream << timestamp << " , " << fixed << setprecision(1);
+		m_ofstream << setw(FIELD_WIDTH) << open << ", ";
         m_ofstream << setw(FIELD_WIDTH) << high << ", ";
         m_ofstream << setw(FIELD_WIDTH) << low << ", ";
         m_ofstream << setw(FIELD_WIDTH) << close << endl;
--- trunk/PTv3/PTStation/HistDataWriter.h
+++ trunk/PTv3/PTStation/HistDataWriter.h
@@ -8,13 +8,16 @@ class CHistDataWriter
 public:
     CHistDataWriter(void);
     ~CHistDataWriter(void);
+	void Init(const string& symbol, int precision, const boost::gregorian::date& tradingDay);
+	bool Open();
     bool Open(const string& symbol, int precision, const boost::gregorian::date& tradingDay);
     bool IsOpen(){ return m_ofstream.is_open(); }
     void Close();
     void Write(const string& timestamp, double open, double high, double low, double close);
-
+	const string& Name() { return m_Name; }
 private:
     string m_symbol;
+	string m_Name;
     unsigned int m_precision;
     boost::filesystem::ofstream m_ofstream;
 };
