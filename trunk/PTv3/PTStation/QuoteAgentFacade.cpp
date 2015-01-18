dd47a43978c3e6cec8698c60221886ee2100c9ce
zxiaoyong@gmail.com <zxiaoyong@gmail.com@5a15556c-d8d4-9b01-ee84-76c2d03371d9>
2015-01-18 15:28:32 +0000

    StaAfx.h

--- trunk/PTv3/PTStation/QuoteAgentFacade.cpp
+++ trunk/PTv3/PTStation/QuoteAgentFacade.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "QuoteAgentFacade.h"
 
 #include <boost/algorithm/string.hpp>
--- trunk/PTv3/PTStation/QuoteUdpAgent.cpp
+++ trunk/PTv3/PTStation/QuoteUdpAgent.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+#include "StdAfx.h"
 #include "QuoteUdpAgent.h"
 
 #include <boost/algorithm/string.hpp>
